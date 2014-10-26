f0e04b7b8b8a93e4fdf1d96a93e8f2bce87b0074
Attila Györffy <attila@attilagyorffy.com>
2014-10-26 19:12:25 +0100

    Initial Commit

---
+++ Mixtapes/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Mixtapes
+//
+//  Created by Attila Györffy on 26/10/14.
+//  Copyright (c) 2014 mixtapes. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MixtapesTests/MixtapesTests.swift
@@ -0,0 +1,36 @@
+//
+//  MixtapesTests.swift
+//  MixtapesTests
+//
+//  Created by Attila Györffy on 26/10/14.
+//  Copyright (c) 2014 mixtapes. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class MixtapesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
